9ff5873910adf1f5727c1d50e59153e55f977469
junyan2011 <junyangao.work@gmail.com>
2017-08-11 00:00:36 -0700

    test

--- app/controllers/authVerifController.js
+++ app/controllers/authVerifController.js
@@ -1,35 +1,39 @@
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const secret = require('../../config/secret').secret;
-const User = require('../models/user'); // get our mongoose model
 
-module.exports.verify = function (req, res, next) {
-    // find the user
-    //console.log(req);
+function verify(req, res, next) {
+    // check header or url parameters or post parameters for token
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    // console.log('junyan is here');
+    console.log(token);
 
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
     // decode token
     if (token) {
+        console.log('detect token');
 
         // verifies secret and checks exp
-        jwt.verify(token, secret, function (err, decoded) {
+        jwt.verify(token, secret, (err, decoded) => {
             if (err) {
-                return res.json({ success: false, message: 'Failed to authenticate token.' });
+                console.log(err);
+                res.json({ success: false, message: 'Failed to authenticate token.' });
             } else {
                 // if everything is good, save to request for use in other routes
                 req.decoded = decoded;
+                console.log('redirect to', req.decoded);
                 next();
+                res.json({ success: true, message: 'Passed to authenticate token.' });
             }
         });
-
     } else {
-
         // if there is no token
         // return an error
-        return res.status(403).send({
+        res.status(403).json({
             success: false,
-            message: 'No token provided.'
+            message: 'No token provided.',
         });
-
     }
-
 }
+
+module.exports = {
+    verify,
+};
